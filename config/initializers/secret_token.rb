00e25a5b9912aee897ba087dce7a15b208c52578
Matt Moore <mmoore@groupon.com>
2013-12-16 18:21:39 -0600

    fresh start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chimerror::Application.config.secret_key_base = 'd121c8d1099c3a37b1a49dd5245e67d18f077a210e074842f69eb96fe372909fd7aa95b1fdfbb9f2c687a94af92163cf2098384a9ba099f79bc9c5928d19260c'
